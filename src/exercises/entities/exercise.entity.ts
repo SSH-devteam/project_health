d74b9242f393b60720ddd051d7575605c8c3a0c8
Hoo <sksk8922@gmail.com>
2023-06-28 01:20:08 +0900

    [수정] exercise enum 수정/ exercise Entity 에 추가

--- src/exercises/entities/exercise.entity.ts
+++ src/exercises/entities/exercise.entity.ts
@@ -1,28 +1,39 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
-import { ExerciseTool } from "../exercise.enum";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
 @Unique(['name'])
 export class Exercise extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-    @PrimaryGeneratedColumn('increment')
-    id:number
+  @Column()
+  name: string;
 
-    @Column()
-    name:string;
+  @Column()
+  tool: ExerciseTool;
 
-    @Column()
-    tool:string;
+  @Column()
+  mainTarget: string;
 
-    @Column()
-    mainTarget:string
+  @Column()
+  subTarget: string;
 
-    @Column()
-    subTarget:string
+  @Column()
+  createdAt: string;
 
-    @Column()
-    createdAt:string
+  @Column()
+  updatedAt: string;
+}
 
-    @Column()
-    updatedAt:string
+export enum ExerciseTool {
+  '바벨',
+  '덤벨',
+  '머신',
+  '맨몸',
 }
